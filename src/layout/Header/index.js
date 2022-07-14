28d10174c735f9004270394b1e0cecc3e2b39b9e
tom harcker <thackerstudent@gmail.com>
2022-07-14 15:55:17 +0100

    stash

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,30 +1,49 @@
 import './index.css';
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './gith.png'
-import { useNavigate } from 'react-router-dom'
+import logo from './gith.png';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
-    const homePage = 'http://localhost:3000/'
-    const url = window.location.href;
-    console.log(url)
-    const navigate = useNavigate();
+	const homePage = 'http://localhost:3000/';
+	const url = window.location.href;
 
+	const navigate = useNavigate();
 
+	const reverseHandler = () => {
+		navigate(-1);
+	};
 
-    return(
-        <>
-           <nav>
-                <img src ={logo} className="nav-icon" alt="small icon of the react logo"/>
-                <h3 className="nav--logo_text"> GitHub Repo getter</h3>
-                <ul>
-                    <li><Link className="nav-title" to="/">Home</Link> </li>
-                    <li><Link className="nav-title" to="/RepoPage/:id">RepoPage</Link></li>
-                    {url !== homePage && <li className="nav-title"> Back </li>}
-                </ul>
-           </nav> 
-        </>
-    )
+	return (
+		<>
+			<nav>
+				<img
+					src={logo}
+					className='nav-icon'
+					alt='small icon of the react logo'
+				/>
+				<h3 className='nav--logo_text'> GitHub Repo getter</h3>
+				<ul>
+					<li>
+						<Link className='nav-title' to='/'>
+							Home
+						</Link>{' '}
+					</li>
+					<li>
+						<Link className='nav-title' to='/RepoPage/:id'>
+							RepoPage
+						</Link>
+					</li>
+					{url !== homePage && (
+						<li onClick={reverseHandler} className='nav-title'>
+							{' '}
+							Back{' '}
+						</li>
+					)}
+				</ul>
+			</nav>
+		</>
+	);
 }
 
 export default Header;
--- src/pages/RepoPage/index.jsx
+++ src/pages/RepoPage/index.jsx
@@ -1,28 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import UserCard from '../../components/UserCard';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const RepoPage = ({ repos }) => {
 	const { id } = useParams();
 	const [repo, setRepo] = useState('');
 	const [loading, setLoading] = useState(true);
 	const [message, setMessage] = useState();
-	const navigate = useNavigate();
 
 	const getRepo = async (repoId) => {
 		return setRepo(repos.filter((e) => e.id.toString() === repoId)[0]);
 	};
 
 	useEffect(() => {
-
 		try {
 			getRepo(id);
-			setLoading(false);
+			return setLoading(false);
 		} catch (err) {
 			console.log(err);
 			setMessage(err);
 		}
-		// if (!repo) return navigate(-1);
 	}, []);
 
 	return (
